abf2a14d7e9b792c1b739aff8f58eb13167d3464
renwickman <57610485+renwickman@users.noreply.github.com>
2020-03-27 16:33:55 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ useNewUrlParser: true }));
+app.use(bodyParser.json());
+app.use(express.static( __dirname + '/public/dist/public' ));
+
+mongoose.connect('mongodb://localhost/movies');
+
+require("./server/config/routes.js")(app);
+
+app.listen(8000, () => console.log("listening on port 8000"));
